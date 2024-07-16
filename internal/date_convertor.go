12935a3be491b01a51a53c2162d0a087acbdbab0
Alexey B <alexeyb@herdwatch.com>
2024-07-16 13:57:48 +0200

    Fixed GetCurrentDateTime method.

--- internal/date_convertor.go
+++ internal/date_convertor.go
@@ -19,7 +19,7 @@ func DateConvert(date string) string {
 }
 
 func GetCurrentDateTime() string {
-	return time.Now().Format("2006-01-02 00:00:00")
+	return time.Now().Format("2006-01-02 15:04:05")
 }
 
 func IsCorrectDate(date string) bool {
